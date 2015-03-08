e9e0c647eba0a1e4b95c38798a34a83237cd7bd3
idelairre <idelairre@gmail.com>
2015-03-08 12:16:13 -0400

    moves commented out code to the end of the file.

--- movie.list_cleaner.rb
+++ movie.list_cleaner.rb
@@ -1,37 +1,5 @@
 require "csv"
-require "pry"
-
-
-# output = File.new("/data/moviegross.csv")
-
-
-# CSV.open("moviegross.csv", "wb") do |csv|
-#   csv << ["title", "total_gross"]
-# end
-
-# it will need to go line by line matching movies to their gross revenue
-# it will need to assign the movie name to a key "title" and the gross return to a key "total_gross"
-
-# find the characters beyond "GR" and match them to the movie "MV"
-
-# string = File.open("data/business.list", "r") { |f| f.read }
-
-# array1 = string.scrub.scan(title_matcher).inject do |memo, title|
-#   memo << { title: title.join.strip }
-# end
-# # puts array1
-
-# array2 = string.scrub.scan(gross_matcher).inject do |memo, gross|
-# memo << { gross: gross.join.strip }
-
-
-# puts array2
-# end
-# array2 = string.scan(gross_matcher).inject([]) do |memo, gross|
-#   memo << {:gross => gross.gsub("\n", '')}
-# end
-
-# compare against the key value pair, if the value for gross isn't nil, add it. ???
+# require "pry"
 
 def stripper
   title_matcher = /(?<=MV: )(.*?)(?=[(])/
@@ -83,3 +51,30 @@ def stripper
 end
 
 stripper
+
+# output = File.new("/data/moviegross.csv")
+
+# CSV.open("moviegross.csv", "wb") do |csv|
+#   csv << ["title", "total_gross"]
+# end
+
+# it will need to go line by line matching movies to their gross revenue
+# it will need to assign the movie name to a key "title" and the gross return to a key "total_gross"
+
+# find the characters beyond "GR" and match them to the movie "MV"
+
+# string = File.open("data/business.list", "r") { |f| f.read }
+
+# array1 = string.scrub.scan(title_matcher).inject do |memo, title|
+#   memo << { title: title.join.strip }
+# end
+# # puts array1
+
+# array2 = string.scrub.scan(gross_matcher).inject do |memo, gross|
+# memo << { gross: gross.join.strip }
+
+# puts array2
+# end
+# array2 = string.scan(gross_matcher).inject([]) do |memo, gross|
+#   memo << {:gross => gross.gsub("\n", '')}
+# end
