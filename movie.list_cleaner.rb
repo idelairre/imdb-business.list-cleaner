21c412d9fe2a43f143e65ecd209f5ac47b2c2814
idelairre <idelairre@gmail.com>
2015-03-08 03:00:27 -0400

    puts title and corresponding grosses if they are present.

--- movie.list_cleaner.rb
+++ movie.list_cleaner.rb
@@ -18,27 +18,51 @@ require "pry"
 
 
 def title_stripper
-title_matcher = /(?<=MV: )(.*?)(?=[(])/
-# gross_matcher = /(?<=GR: USD )(.*?)(?=[(])/
+  title_matcher = /(?<=MV: )(.*?)(?=[(])/
+  gross_matcher = /(?<=GR: USD )(.*?)(?=[(])/
 
-# binding.pry
+  # string = File.open("data/business.list", "r") { |f| f.read }
 
-  string = File.open("data/business.list", "r") { |f| f.read }
+  # array1 = string.scrub.scan(title_matcher).inject do |memo, title|
+  #   memo << { title: title.join.strip }
+  # end
+  # # puts array1
 
-  array = string.scrub.scan(title_matcher).inject do |memo, title|
-    memo << { title: title.join.strip }
-  end
-  puts array
+  # array2 = string.scrub.scan(gross_matcher).inject do |memo, gross|
+  # memo << { gross: gross.join.strip }
+
+
+  # puts array2
+  # end
   # array2 = string.scan(gross_matcher).inject([]) do |memo, gross|
   #   memo << {:gross => gross.gsub("\n", '')}
   # end
+
+array = []
+
+  File.open("data/business.list", "r").each_line do |line|
+    # grossPresent = false
+    if line.scrub.lstrip.match(title_matcher)
+      title = line.scrub.scan(title_matcher).join.strip
+      puts title
+    end
+    if line.scrub.lstrip.match(gross_matcher)
+        # grossPresent = true
+        gross = line.scrub.scan(gross_matcher)
+        # array << { title: title, gross: gross }
+        puts gross
+      # elsif grossPresent == false
+        # array << { title: title }
+    end
+      # puts array
+  end
 end
 
-title_stripper
+  title_stripper
 
 
-# example code to append to a csv
+  # example code to append to a csv
 
-# CSV.open("moviegross.csv", "a+") do |csv|
-#   csv << ["cow", "3","2500"]
-# end
+  # CSV.open("moviegross.csv", "a+") do |csv|
+  #   csv << ["cow", "3","2500"]
+  # end
